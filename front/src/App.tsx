65c3610ffedf884ae61ceb74aabff8548d176c0c
Yoann Delion <yoanndelion@gmail.com>
2021-07-31 16:36:15 +0200

    front init

---
+++ front/src/App.tsx
@@ -0,0 +1,11 @@
+import Typography from '@material-ui/core/Typography'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Typography variant="h1" component="h2" gutterBottom>
+        h1. Heading
+      </Typography>
+    </div>
+  )
+}
